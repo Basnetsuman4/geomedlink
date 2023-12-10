3fa8289b908744ea12e513eb67fac40d57bff2cd
Suman Basnet <Arunbasnet54@gmail.com>
2023-12-10 13:26:00 +0545

    IN PROGRESS

---
+++ screens/Upload/Upload.js
@@ -0,0 +1,29 @@
+import { View, Text } from "react-native";
+
+const Upload = () => {
+    return (<>
+        <View>
+
+            <View>
+                <Text>Title</Text>
+            </View>
+            <View>
+                <View>
+                    <Text>Upload DP section</Text>
+                </View>
+
+                <View>
+                    <View>
+                        <Text>Upload  Text Section</Text>
+                    </View>
+                    <View>
+                        <Text>Upload  Image Section</Text>
+                    </View>
+                </View>
+            </View>
+
+        </View>
+    </>);
+}
+
+export default Upload;
