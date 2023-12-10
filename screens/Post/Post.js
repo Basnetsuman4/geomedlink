1c359273147ed29c6be855cc9fadbcbfc30bbef5
Suman Basnet <Arunbasnet54@gmail.com>
2023-12-10 18:49:23 +0545

    Post Updates of report

--- screens/Post/Post.js
+++ screens/Post/Post.js
@@ -1,198 +1,106 @@
 // // import { useState, useEffect } from 'react';
-import { Button, Card, Avatar, IconButton } from 'react-native-paper';
-import SignOut from '../../components/modal/SignOut';
+// import { Button, Card, Avatar, IconButton } from 'react-native-paper';
+// import SignOut from '../../components/modal/SignOut';
 
-import { Text, View, ScrollView, Pressable, StyleSheet } from "react-native";
-import Icon from 'react-native-vector-icons/FontAwesome5'
-import Iconll from 'react-native-vector-icons/AntDesign'
+// import { Text, View, ScrollView, Pressable, StyleSheet } from "react-native";
+// import Icon from 'react-native-vector-icons/FontAwesome5'
+// import Iconll from 'react-native-vector-icons/AntDesign'
+// import Report from '../../components/modal/Report';
 
 
 
-// import React, { useState, useEffect, useRef } from 'react';
-// import { Text, View, ScrollView, StyleSheet } from 'react-native';
-// import { Card, Avatar } from 'react-native-paper';
-// import axios from 'axios';
 
-// export default function Post() {
-//     const [postdata, setPostData] = useState([]);
-//     const [userName, setUserName] = useState('Dummy');
-//     const [pageNumber, setPageNumber] = useState(1);
-//     const scrollViewRef = useRef(null);
+// const Post = () => {
+//     return (<>
 
-//     useEffect(() => {
-//         getData();
-//         fetchUserName();
-//     }, []);
-
-//     const BaseUrl = `http://localhost:8000/data?page=${pageNumber}&limit=10`;
-
-//     // function getData() {
-//     //     axios.get(BaseUrl)
-//     //         .then(response => {
-//     //             setPostData(prevData => [...prevData, ...response.data]);
-//     //         })
-//     //         .catch(error => {
-//     //             console.error('Error fetching data:', error);
-//     //         });
-//     // }
-
-//     // function fetchUserName() {
-//     //     axios.get('https://api.example.com/user')
-//     //         .then(response => {
-//     //             const userFirstName = response.data.firstName;
-//     //             setUserName(userFirstName);
-//     //         })
-//     //         .catch(error => {
-//     //             console.error('Error fetching user data:', error);
-//     //         });
-//     // }
-
-//     const handleScroll = (event) => {
-//         const { layoutMeasurement, contentOffset, contentSize } = event.nativeEvent;
-//         const paddingToBottom = 20;
-//         if (layoutMeasurement.height + contentOffset.y >= contentSize.height - paddingToBottom) {
-//             setPageNumber(prevPageNumber => prevPageNumber + 1);
-//             getData();
-//         }
-//     };
-
-//     return (
-//         <ScrollView
-//             ref={scrollViewRef}
-//             contentContainerStyle={styles.PostContainer}
-//             onScroll={handleScroll}
-//             scrollEventThrottle={16}
-//         >
-//             {postdata.map((post, index) => (
-//                 <Card key={index} style={styles.Post}>
+//         <ScrollView contentContainerStyle={styles.PostContainer}>
+//             <Card style={styles.Post} >
+//                 <View style={styles.postTop}>
 //                     <View style={styles.dpName}>
-//                         <Avatar.Image size={32} source={require('../../assets/img/mydp.png')} />
-//                         <Text style={styles.Name}>
-//                             \jzlkx
-//                             {/* {userName} */}
-//                         </Text>
+//                         <Avatar.Image size={40} source={require('../../assets/img/Harry.png')} />
+//                         <Text style={styles.Name}>Hobart Romain Alex</Text>
 //                     </View>
+//                     <Pressable onPress={() =>
+//                         <Report />
+//                     }>
+//                         <Icon name="ellipsis-v" size={18} color="grey" style={styles.optionIcon} />
+//                     </Pressable>
+//                 </View>
+//                 <Card.Content>
+//                     <Text variant="titleLarge" style={styles.caption}>
+//                         Lately, it feels like I've been in a never-ending tango with this stubborn bug! The constant sniffles, sneezing symphonies, and feeling like a marathon runner with a cough have been my daily companions. Slowly pacing through each day, holding out hope for the day this bug decides to bid adieu! 🤒🌧️ </Text>
+//                 </Card.Content>
+//                 <View style={styles.ImageBox}>
+//                     <Card.Cover source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzJRDYPQYpItOt9krno_-3JLQ32qQMDfEwUQ&usqp=CAU' }} style={styles.Image} />
+//                 </View>
+//                 <Card.Actions >
+//                     <View style={styles.Interactive}>
+//                         {/* <Button style={styles.Button}>Like</Button> */}
+//                         {/* <Iconll name="" size={} color: */}
+//                         <Iconll name="like2" size={18} style={styles.reactionIcon} />
+//                         <Icon name="comment-alt" size={18} style={styles.reactionIcon} />
+//                         <Icon name="share-square" size={18} style={styles.reactionIcon} />
 
-//                     {/* <Text>{post.title}</Text>
-//                     <Text>{post.description}</Text> */}
-//                 </Card>
-//             ))}
+//                     </View>
+//                 </Card.Actions>
+//             </Card>
+
 //         </ScrollView>
-//     );
-// }
 
+//     </>);
+// }
+import React, { useState } from 'react';
+import { View, Text, ScrollView, Pressable, StyleSheet, Modal } from 'react-native';
+import { Card, Avatar } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import Iconll from 'react-native-vector-icons/AntDesign';
+import Report from '../../components/modal/Report';
 
 const Post = () => {
-    return (<>
+    const [reportModalVisible, setReportModalVisible] = useState(false);
 
-        <ScrollView contentContainerStyle={styles.PostContainer}>
-            <Card style={styles.Post} >
-                <View style={styles.postTop}>
-                    <View style={styles.dpName}>
-                        <Avatar.Image size={40} source={require('../../assets/img/Harry.png')} />
-                        <Text style={styles.Name}>Hobart Romain Alex</Text>
+    const toggleReportModal = () => {
+        setReportModalVisible(!reportModalVisible);
+    };
+
+    return (
+        <>
+            <ScrollView contentContainerStyle={styles.PostContainer}>
+                <Card style={styles.Post}>
+                    <View style={styles.postTop}>
+                        <View style={styles.dpName}>
+                            <Avatar.Image size={40} source={require('../../assets/img/Harry.png')} />
+                            <Text style={styles.Name}>Hobart Romain Alex</Text>
+                        </View>
+                        <Pressable onPress={toggleReportModal} style={styles.optionIcon}>
+                            <Icon name="ellipsis-v" size={18} color="grey" />
+                        </Pressable>
                     </View>
-                    <Pressable onPress={() =>
-                        // console.log('her bhai')
-                        <SignOut />
-                    }>
-                        {/* <Avatar.Text size={28} label="!" /> */}
-                        <Icon name="ellipsis-v" size={18} color="grey" style={styles.optionIcon} />
-
-
-                    </Pressable>
-                </View>
-                <Card.Content>
-                    <Text variant="titleLarge" style={styles.caption}>
-                        Lately, it feels like I've been in a never-ending tango with this stubborn bug! The constant sniffles, sneezing symphonies, and feeling like a marathon runner with a cough have been my daily companions. Slowly pacing through each day, holding out hope for the day this bug decides to bid adieu! 🤒🌧️ </Text>
-                </Card.Content>
-                <View style={styles.ImageBox}>
-                    <Card.Cover source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzJRDYPQYpItOt9krno_-3JLQ32qQMDfEwUQ&usqp=CAU' }} style={styles.Image} />
-                </View>
-                <Card.Actions >
-                    <View style={styles.Interactive}>
-                        {/* <Button style={styles.Button}>Like</Button> */}
-                        {/* <Iconll name="" size={} color: */}
-                        <Iconll name="like2" size={18} style={styles.reactionIcon} />
-                        <Icon name="comment-alt" size={18} style={styles.reactionIcon} />
-                        <Icon name="share-square" size={18} style={styles.reactionIcon} />
-
+                    <Card.Content>
+                        <Text variant="titleLarge" style={styles.caption}>
+                            Lately, it feels like I've been in a never-ending tango with this stubborn bug! The constant sniffles, sneezing symphonies, and feeling like a marathon runner with a cough have been my daily companions. Slowly pacing through each day, holding out hope for the day this bug decides to bid adieu! 🤒🌧️ </Text>
+                    </Card.Content>
+                    <View style={styles.ImageBox}>
+                        <Card.Cover source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzJRDYPQYpItOt9krno_-3JLQ32qQMDfEwUQ&usqp=CAU' }} style={styles.Image} />
                     </View>
-                </Card.Actions>
-            </Card>
-            <Card style={styles.Post} >
-                <View style={styles.postTop}>
-                    <View style={styles.dpName}>
-                        <Avatar.Image size={40} source={require('../../assets/img/Harry.png')} />
-                        <Text style={styles.Name}>Hobart Romain Alex</Text>
-                    </View>
-                    <Pressable onPress={() =>
-                        // console.log('her bhai')
-                        <SignOut />
-                    }>
-                        {/* <Avatar.Text size={28} label="!" /> */}
-                        <Icon name="ellipsis-v" size={18} color="grey" style={styles.optionIcon} />
+                    <Card.Actions >
+                        <View style={styles.Interactive}>
+                            {/* <Button style={styles.Button}>Like</Button> */}
+                            {/* <Iconll name="" size={} color: */}
+                            <Iconll name="like2" size={18} style={styles.reactionIcon} />
+                            <Icon name="comment-alt" size={18} style={styles.reactionIcon} />
+                            <Icon name="share-square" size={18} style={styles.reactionIcon} />
 
-
-                    </Pressable>
-                </View>
-                <Card.Content>
-                    <Text variant="titleLarge" style={styles.caption}>
-                        Lately, it feels like I've been in a never-ending tango with this stubborn bug! The constant sniffles, sneezing symphonies, and feeling like a marathon runner with a cough have been my daily companions. Slowly pacing through each day, holding out hope for the day this bug decides to bid adieu! 🤒🌧️ </Text>
-                </Card.Content>
-                <View style={styles.ImageBox}>
-                    <Card.Cover source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzJRDYPQYpItOt9krno_-3JLQ32qQMDfEwUQ&usqp=CAU' }} style={styles.Image} />
-                </View>
-                <Card.Actions >
-                    <View style={styles.Interactive}>
-                        {/* <Button style={styles.Button}>Like</Button> */}
-                        {/* <Iconll name="" size={} color: */}
-                        <Iconll name="like2" size={18} style={styles.reactionIcon} />
-                        <Icon name="comment-alt" size={18} style={styles.reactionIcon} />
-                        <Icon name="share-square" size={18} style={styles.reactionIcon} />
-
-                    </View>
-                </Card.Actions>
-            </Card>
-            <Card style={styles.Post} >
-                <View style={styles.postTop}>
-                    <View style={styles.dpName}>
-                        <Avatar.Image size={40} source={require('../../assets/img/Harry.png')} />
-                        <Text style={styles.Name}>Hobart Romain Alex</Text>
-                    </View>
-                    <Pressable onPress={() =>
-                        // console.log('her bhai')
-                        <SignOut />
-                    }>
-                        {/* <Avatar.Text size={28} label="!" /> */}
-                        <Icon name="ellipsis-v" size={18} color="grey" style={styles.optionIcon} />
-
-
-                    </Pressable>
-                </View>
-                <Card.Content>
-                    <Text variant="titleLarge" style={styles.caption}>
-                        Lately, it feels like I've been in a never-ending tango with this stubborn bug! The constant sniffles, sneezing symphonies, and feeling like a marathon runner with a cough have been my daily companions. Slowly pacing through each day, holding out hope for the day this bug decides to bid adieu! 🤒🌧️ </Text>
-                </Card.Content>
-                <View style={styles.ImageBox}>
-                    <Card.Cover source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzJRDYPQYpItOt9krno_-3JLQ32qQMDfEwUQ&usqp=CAU' }} style={styles.Image} />
-                </View>
-                <Card.Actions >
-                    <View style={styles.Interactive}>
-                        {/* <Button style={styles.Button}>Like</Button> */}
-                        {/* <Iconll name="" size={} color: */}
-                        <Iconll name="like2" size={18} style={styles.reactionIcon} />
-                        <Icon name="comment-alt" size={18} style={styles.reactionIcon} />
-                        <Icon name="share-square" size={18} style={styles.reactionIcon} />
-
-                    </View>
-                </Card.Actions>
-            </Card>
-        </ScrollView>
-
-    </>);
-}
-
+                        </View>
+                    </Card.Actions>
+                </Card>
+                <Modal visible={reportModalVisible} transparent animationType="slide">
+                    <Report visible={reportModalVisible} hideModal={toggleReportModal} />
+                </Modal>
+            </ScrollView>
+        </>
+    );
+};
 export default Post;
 const styles = StyleSheet.create({
     PostContainer: {
@@ -283,7 +191,6 @@ const styles = StyleSheet.create({
     },
     ImageBox: {
         width: "100%",
-        // backgroundColor: 'red',
         justifyContent: 'center',
         paddingRight: 15,
         paddingLeft: 15,
@@ -291,10 +198,92 @@ const styles = StyleSheet.create({
     optionIcon: {
         paddingTop: 10,
         // backgroundColor: 'red',
-        width: 15
+        width: 40,
+        alignItems: 'center'
     },
     reactionIcon: {
         color: "grey",
     }
 
 })
+
+
+
+
+
+
+
+
+
+// import React, { useState, useEffect, useRef } from 'react';
+// import { Text, View, ScrollView, StyleSheet } from 'react-native';
+// import { Card, Avatar } from 'react-native-paper';
+// import axios from 'axios';
+
+// export default function Post() {
+//     const [postdata, setPostData] = useState([]);
+//     const [userName, setUserName] = useState('Dummy');
+//     const [pageNumber, setPageNumber] = useState(1);
+//     const scrollViewRef = useRef(null);
+
+//     useEffect(() => {
+//         getData();
+//         fetchUserName();
+//     }, []);
+
+//     const BaseUrl = `http://localhost:8000/data?page=${pageNumber}&limit=10`;
+
+//     // function getData() {
+//     //     axios.get(BaseUrl)
+//     //         .then(response => {
+//     //             setPostData(prevData => [...prevData, ...response.data]);
+//     //         })
+//     //         .catch(error => {
+//     //             console.error('Error fetching data:', error);
+//     //         });
+//     // }
+
+//     // function fetchUserName() {
+//     //     axios.get('https://api.example.com/user')
+//     //         .then(response => {
+//     //             const userFirstName = response.data.firstName;
+//     //             setUserName(userFirstName);
+//     //         })
+//     //         .catch(error => {
+//     //             console.error('Error fetching user data:', error);
+//     //         });
+//     // }
+
+//     const handleScroll = (event) => {
+//         const { layoutMeasurement, contentOffset, contentSize } = event.nativeEvent;
+//         const paddingToBottom = 20;
+//         if (layoutMeasurement.height + contentOffset.y >= contentSize.height - paddingToBottom) {
+//             setPageNumber(prevPageNumber => prevPageNumber + 1);
+//             getData();
+//         }
+//     };
+
+//     return (
+//         <ScrollView
+//             ref={scrollViewRef}
+//             contentContainerStyle={styles.PostContainer}
+//             onScroll={handleScroll}
+//             scrollEventThrottle={16}
+//         >
+//             {postdata.map((post, index) => (
+//                 <Card key={index} style={styles.Post}>
+//                     <View style={styles.dpName}>
+//                         <Avatar.Image size={32} source={require('../../assets/img/mydp.png')} />
+//                         <Text style={styles.Name}>
+//                             \jzlkx
+//                             {/* {userName} */}
+//                         </Text>
+//                     </View>
+
+//                     {/* <Text>{post.title}</Text>
+//                     <Text>{post.description}</Text> */}
+//                 </Card>
+//             ))}
+//         </ScrollView>
+//     );
+// }
