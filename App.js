44d75a3838eeab6121d68b003fb150b146ad2eb4
Suman Basnet <Arunbasnet54@gmail.com>
2023-12-10 13:26:39 +0545

    App file

--- App.js
+++ App.js
@@ -4,14 +4,18 @@ import Navigation from './navigation/Navigation'
 import ProfileScreen from './screens/Profile/ProfileScreen'
 import { Provider } from 'react-native-paper'
 import Post from './screens/Post/Post'
+import News from './screens/News/News'
+import Upload from './screens/Upload/Upload'
 const App = () => {
     return (
         <Provider>
-        <View>
-            {/* <ProfileScreen /> */}
-            <Post />
-            {/* <Navigation /> */}
-        </View>
+            <View>
+                {/* <News /> */}
+                {/* <ProfileScreen /> */}
+                {/* <Post /> */}
+                {/* <Navigation /> */}
+                <Upload />
+            </View>
         </Provider>
     )
 }
