67c27c3b0fdeb64a0e13da80a971e99000c462a3
Suman Basnet <Arunbasnet54@gmail.com>
2023-12-13 21:53:49 +0545

    Change Password Screen

--- screens/ChangePsw/ChangePsw.js
+++ screens/ChangePsw/ChangePsw.js
@@ -1,91 +1,123 @@
-import { View, StyleSheet } from "react-native";
-import { Avatar, TextInput, Button, Text } from "react-native-paper";
-import styles from "../../components/modal/stylings/ModalStyling";
+import { View, StyleSheet, Image, Text, TextInput } from "react-native";
+import { Button } from "react-native-paper";
 import { useState } from "react";
 
-
 const ChangePsw = () => {
-
-    const [visible, setVisible] = useState(false);
-    const [text, setText] = useState('');
-
-    const showModal = () => setVisible(true);
-    const hideModal = () => setVisible(false);
+    const [currentPassword, setCurrentPassword] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
 
+    const handleChangePassword = () => {
 
+        const data = {
+            currentPassword: currentPassword,
+            newPassword: newPassword,
+            confirmPassword: confirmPassword
+        };
+
+
+        axios.post('BASEurl', data)
+            .then(response => {
+                console.log("Password changed successfully", response.data);
+            })
+            .catch(error => {
+                console.error("Error changing password", error);
+            });
+    };
     return (<>
         <View style={styles.Mainbox}>
-
-
-
             <View style={styles.changePswrdBox}>
+                <Text style={{ fontSize: 24, fontWeight: "bold", textAlign: 'center', marginBottom: 20, marginTop: 10 }}>Reset Password</Text>
+                <View style={styles.imgBox}>
+                    <Image
+                        source={require('../../assets/icon/lockk.png')}
+                        style={styles.image}
+                    />
+                </View>
                 <View style={styles.interactiveSection}>
                     <View style={styles.inputSection}>
-                        <Text>Old Password</Text>
                         <TextInput
-                            placeholder='Example: qwert@123'
+                            style={styles.data}
+                            placeholder='Current Password'
                             secureTextEntry={true}
                             mode='outlined'
                             type='password'
+                            value={currentPassword}
+                            onChangeText={text => setCurrentPassword(text)}
                         />
                     </View>
                     <View style={styles.inputSection}>
-                        <Text>New Password</Text>
                         <TextInput
+                            style={styles.data}
                             placeholder='New Password'
                             mode='outlined'
                             secureTextEntry={true}
+                            value={newPassword}
+                            onChangeText={text => setNewPassword(text)}
                         />
                     </View>
                     <View style={styles.inputSection}>
-                        <Text>Confirm New Password</Text>
                         <TextInput
+                            style={styles.data}
                             placeholder='Re-enter New Password'
                             secureTextEntry={true}
                             mode='outlined'
+                            value={confirmPassword}
+                            onChangeText={text => setConfirmPassword(text)}
+
                         />
                     </View>
                 </View>
             </View>
-
-
-
-            {/* <View style={styles.dpBox}>
-                <Avatar.Image size={120}
-                    style={styles.dp}
-                    source={require('../../assets/img/hobert.webp')}
-                />
-                <Text style={styles.fullName}>Hobert Romain Alex</Text>
-            </View>
-
-            <View style={styles.detailBox}>
-                <View style={styles.keys}>
-                    <Text style={styles.keyTitle}>FullName: </Text>
-                    <Text style={styles.keyTitle}>Email: </Text>
-                    <Text style={styles.keyTitle}>Address: </Text>
-                    <Text style={styles.keyTitle}>Contact: </Text>
-                    <Text style={styles.keyTitle}>Blood Group:  </Text>
-                </View>
-                <View style={styles.values}>
-                    <Text style={styles.keyTitle}>Dummy Bahadur Koirala</Text>
-                    <Text style={styles.keyTitle}>Dummy123@gmail.com</Text>
-                    <Text style={styles.keyTitle}>Balkhu, Kathmandu</Text>
-                    <Text style={styles.keyTitle}>1234659875</Text>
-                    <Text style={styles.keyTitle}>AB'-ve'</Text>
-                </View>
-            </View>
-            <View style={styles.editButton}>
-                <Button title="Edit Details">d</Button>
-            </View> */}
-
-
             <View style={styles.pswBtn}>
-                <Button>Confirm Change</Button>
+                <Button style={styles.btnpsw} labelStyle={{ color: 'white' }} onPress={handleChangePassword}>Confirm Change</Button>
             </View>
-        </View>
+
+        </View >
 
     </>);
 }
+
+
+const styles = StyleSheet.create({
+    Mainbox: {
+        height: '100%',
+        backgroundColor: '#7FFFD4',
+        paddingTop: 10,
+        paddingHorizontal: 15
+    },
+    data: {
+        backgroundColor: 'white',
+        borderRadius: 10
+    },
+    interactiveSection: {
+        // backgroundColor: 'red',
+        marginTop: 20,
+        height: '50%',
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    inputSection: {
+        height: '35%',
+        // backgroundColor: 'blue',
+        justifyContent: 'space-around'
+    },
+    image: {
+        width: "100%",
+        height: 100,
+        justifyContent: 'center',
+        resizeMode: 'contain', // Adjust the image size and aspect ratio
+    },
+    btnpsw: {
+        width: '60%',
+        backgroundColor: '#7B66FF',
+        margin: 'auto',
+        alignSelf: 'center',
+    }, pswBtn: {
+        width: "100%",
+        justifyContent: "center",
+    },
+})
 
 export default ChangePsw;
