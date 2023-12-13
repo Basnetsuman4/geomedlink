f9609926c8eb9adb8444c4a55d4c3e82f737e71d
Suman Basnet <Arunbasnet54@gmail.com>
2023-12-13 07:31:26 +0545

    Updated Drawer

--- screens/Profile/ProfileScreen.js
+++ screens/Profile/ProfileScreen.js
@@ -1,90 +1,86 @@
-// import { ScrollView, View, StyleSheet, Pressable } from 'react-native'
-// import React, { useState } from 'react'
-// import { Text, Avatar } from 'react-native-paper'
-// import DetailModal from '../../components/modal/DetailModal'
-// import PasswordModal from '../../components/modal/PasswordModal'
-// import styles from '../../components/modal/stylings/ModalStyling'
-// import SignOut from '../../components/modal/SignOut'
-// import Post from '../Post/Post'
-
-// const ProfileScreen = () => {
-
-//     return (
-//         // View of whole screen
-//         <View style={styles.Wscreen}>
-//             <Pressable style={styles.ham} onPress={() => {
-//                 console.log('Ham pressed!!!');
-//             }}>
-//                 <Avatar.Icon size={20} icon="folder" />
-//             </Pressable>
-//             <View style={styles.dpBox}>
-
-//                 <Avatar.Image size={120}
-//                     style={styles.dp}
-//                     source={require('../../assets/img/mydp.png')}
-//                 />
-//                 <Text style={styles.fullName}>Dummy Bahadur</Text>
-
-//             </View>
-//             {/* <DetailModal />
-//             <PasswordModal />
-//             <SignOut /> */}
-//             <View style={{ padding: 10, fontstyle: 'bold', fontSize: 50 }}>
-//                 <Text>My Posts</Text>
-//                 <Post />
-//             </View>
-//         </View >
-//     )
-// }
-
-// export default ProfileScreen;
-
-
-import React, { useState, useEffect } from 'react';
-import { View, Text, Avatar } from 'react-native';
-import axios from 'axios';
 import styles from '../../components/modal/stylings/ModalStyling';
+import React, { useState } from 'react';
+import { View, StyleSheet, Pressable, Modal, Dimensions, TouchableWithoutFeedback } from 'react-native';
+import { Text, Avatar } from 'react-native-paper';
+import DetailModal from '../../components/modal/DetailModal';
+import PasswordModal from '../../components/modal/PasswordModal';
+import SignOut from '../../components/modal/SignOut';
+import Post from '../Post/Post';
+import Iconll from 'react-native-vector-icons/AntDesign';
 
 const ProfileScreen = () => {
-    const [firstName, setFirstName] = useState('Dummy Bahadur');
+    const [drawerVisible, setDrawerVisible] = useState(false);
 
-    useEffect(() => {
+    const toggleDrawer = () => {
+        setDrawerVisible(!drawerVisible);
+    };
 
-        axios.get('https://api.example.com/user')
-            .then(response => {
+    const closeDrawer = () => {
+        setDrawerVisible(false);
+    };
 
-                const userFirstName = response.data.firstName;
-                setFirstName(userFirstName);
-            })
-            .catch(error => {
-                console.error('Error fetching user data:', error);
-            });
-    }, []);
     return (
         <View style={styles.Wscreen}>
-            <Pressable style={styles.ham} onPress={() => {
-                console.log('Ham pressed!!!');
-            }}>
-                <Avatar.Icon size={20} icon="folder" />
+            {/* Header */}
+            <Pressable style={styles.ham} onPress={toggleDrawer}>
+                <Iconll name="menuunfold" size={20} style={{ transform: [{ rotate: '180deg' }] }} />
             </Pressable>
+
+            {/* Profile Content */}
             <View style={styles.dpBox}>
-                <Avatar.Image size={120}
+                <Avatar.Image
+                    size={120}
                     style={styles.dp}
-                    source={require('../../assets/img/mydp.png')}
+                    source={require('../../assets/img/hobert.webp')}
                 />
-                <Text style={styles.fullName}>{firstName} Bahadur</Text>
+                <Text style={styles.fullName}>Hobert Romain Alex</Text>
             </View>
 
-            {/* Modal components  ho yo, routing bata milauna parxa yeslai*/}
-            {/* <DetailModal />
-            <PasswordModal />
-            <SignOut /> */}
-            <View style={{ padding: 10, fontWeight: 'bold', fontSize: 50 }}>
-                <Text>My Posts</Text>
+            {/* Drawer */}
+            <Modal
+                visible={drawerVisible}
+                animationType="fade"
+                transparent={true}
+                onRequestClose={closeDrawer}
+            >
+                <TouchableWithoutFeedback onPress={closeDrawer}>
+                    <View style={styless.drawerOverlay}>
+                        <View style={styless.drawer}>
+                            {/* Content inside the drawer */}
+                            <DetailModal />
+                            <PasswordModal />
+                            <SignOut />
+                        </View>
+                    </View>
+                </TouchableWithoutFeedback>
+            </Modal>
+
+            {/* Other profile content */}
+            <View style={styles.postContainer}>
+                {/* <Text style={{ fontWeight: 'bold', fontSize: 20 }}>My Posts</Text> */}
                 <Post />
             </View>
         </View>
     );
 };
+
+const styless = StyleSheet.create({
+
+    drawer: {
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        height: '100%',
+        width: Dimensions.get('window').width * 0.8, // Occupies 80% of the screen width
+        backgroundColor: 'white',
+
+    },
+
+    drawerOverlay: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent background to cover the screen
+        zIndex: 1
+    },
+});
 
 export default ProfileScreen;
